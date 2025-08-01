c50d4938c7016abc9471da82ec093f5c858aa5a5
Vyakart <nishit@tuta.io>
2025-08-02 00:45:13 +0530

    Replace bento grid with accessible GIF-on-active poster carousel; styles and JS logic added

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CircularTestimonialsDemo } from "./components/ui/demo";
+
+function App() {
+  return (
+    <main className="min-h-screen w-full flex items-center justify-center p-6">
+      <CircularTestimonialsDemo />
+    </main>
+  );
+}
+
+export default App;
